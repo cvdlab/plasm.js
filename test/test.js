71b9ab701c6eb61a09d4631026ed168c2f52f5fa
Enrico Marino <enrico.marino@email.com>
2012-04-06 12:19:55 +0300

    Update test/test.js

--- test/test.js
+++ test/test.js
@@ -1,1 +1,12 @@
 console.log('test');
+
+var load = function (id, n) {
+  var url = "https://raw.github.com/cvdlab-cg/" + id 
+    + "/master/2012-04-03/exercise" + n + ".js";
+
+  var script = document.createElement('script');
+  script.src = url;
+  document.body.appendChild(script);
+
+  return url;
+};
